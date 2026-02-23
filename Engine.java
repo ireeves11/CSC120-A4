04cdf5783a88db3a304305c5666f46482570b95f
ireeves11 <ireeves@smith.edu>
2026-02-23 14:20:32 -0500

    Build's Engine Class

--- Engine.java
+++ Engine.java
@@ -1,3 +1,33 @@
 public class Engine {
+    private FuelType fuel;
+    private double maxFuel;
+    private double currentFuel;
 
+    public Engine (FuelType fuel, double current, double max){
+        this.fuel = fuel;
+        maxFuel = max;
+        currentFuel = current;
+    }
+
+    public FuelType getFuelType(){
+        return fuel;
+    }
+
+    public double getMaxFuel(){
+        return maxFuel;
+    }
+
+    double getCurrentFuel(){
+        return currentFuel;
+    }
+    void refuel(){
+        currentFuel = maxFuel;
+    } 
+    Boolean go(){
+        if (currentFuel > 0){
+            currentFuel = currentFuel - 1;
+        }
+        System.out.println("Fuel Remaining: " + currentFuel);
+        return currentFuel > 0;
+    }
 }
